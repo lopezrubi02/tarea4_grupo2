34145d649a3c19b4ad59d48697eb9ceb4e2c90f5
Rubi Polanco <a20183238@pucp.edu.pe>
2021-05-03 15:21:06 -0500

    registrar usuario (los que hagan pull combinen ambos avances)

--- src/main/java/com/example/tarea4_grupo2/controller/UsuarioController.java
+++ src/main/java/com/example/tarea4_grupo2/controller/UsuarioController.java
@@ -4,10 +4,11 @@ import com.example.tarea4_grupo2.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/usuario")
+@RequestMapping("/cliente")
 public class UsuarioController {
 
     @Autowired
@@ -17,5 +18,11 @@ public class UsuarioController {
     public String nuevoUsuario(){
         return "cliente/registroCliente";
     }
+
+    @PostMapping("/guardarNuevo")
+    public String guardarUsuario(){
+        return "redirect:/cliente/nuevo";
+    }
+
 
 }
