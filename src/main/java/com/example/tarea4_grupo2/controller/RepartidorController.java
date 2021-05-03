7a8b45896a61d9c3b9346674598db2dc21222388
sarita atanacio <atanacio.sarita@pucp.pe>
2021-05-03 17:16:17 -0500

    repartidor modificaciones

---
+++ src/main/java/com/example/tarea4_grupo2/controller/RepartidorController.java
@@ -0,0 +1,50 @@
+package com.example.tarea4_grupo2.controller;
+
+import com.example.tarea4_grupo2.entity.Pedidos;
+import com.example.tarea4_grupo2.entity.Restaurante;
+import com.example.tarea4_grupo2.repository.DireccionesRepository;
+import com.example.tarea4_grupo2.repository.RepartidorRepository;
+import com.example.tarea4_grupo2.repository.RestauranteRepository;
+import com.example.tarea4_grupo2.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/repartidor")
+public class RepartidorController {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Autowired
+    RepartidorRepository repartidorRepository;
+
+    @Autowired
+    DireccionesRepository direccionesRepository;
+
+    @Autowired
+    RestauranteRepository restauranteRepository;
+
+    @PostMapping("/Reporte1")
+    public String buscaxRestauranteDistrito(@RequestParam("valorBuscado") String searchField,
+                                      Model model) {
+
+        List<Pedidos> listaPedidosxRestaurante = repartidorRepository.findPedidosByRestaurante(searchField);
+        model.addAttribute("listaPedidosxDistrito", listaPedidosxRestaurante);
+
+        List<Pedidos> listaPedidosxDistrito = repartidorRepository.findPedidosByDistrito(searchField);
+        model.addAttribute("listaPedidosxDistrito", listaPedidosxDistrito);
+
+        return "repartidor/reporte1";
+    }
+
+
+
+
+}
--- src/main/java/com/example/tarea4_grupo2/repository/RepartidorRepository.java
+++ src/main/java/com/example/tarea4_grupo2/repository/RepartidorRepository.java
@@ -1,9 +1,26 @@
 package com.example.tarea4_grupo2.repository;
 
+import com.example.tarea4_grupo2.entity.Pedidos;
 import com.example.tarea4_grupo2.entity.Repartidor;
+import com.example.tarea4_grupo2.entity.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RepartidorRepository  extends JpaRepository<Repartidor, Integer> {
     Repartidor findRepartidorByUsuariosIdusuariosEquals(int idusuario);
+
+    @Query(value = "select p.idpedidos, p.montototal, p.comisionrepartidor, p.calificacionrepartidor, r.nombre, r.distrito\n" +
+            "from pedidos p\n" +
+            "inner join restaurante r on (p.restaurante_idrestaurante=r.idrestaurante) where r.nombre like CONCAT(?1,'%')",
+            nativeQuery = true)
+    List<Pedidos> findPedidosByRestaurante(String nombre);
+
+    @Query(value = "select p.idpedidos, p.montototal, p.comisionrepartidor, p.calificacionrepartidor, r.nombre, r.distrito\n" +
+            "from pedidos p\n" +
+            "inner join restaurante r on (p.restaurante_idrestaurante=r.idrestaurante) where r.distrito like CONCAT(?1,'%')",
+            nativeQuery = true)
+    List<Pedidos> findPedidosByDistrito(String distrito);
 
 }
