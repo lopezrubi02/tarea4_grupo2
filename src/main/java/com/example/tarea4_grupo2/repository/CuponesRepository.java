a6e597a3d6cd2a5c0ee908a6622a9c4d7d7332ef
joseluismunozzuta <joseluismunozzuta@gmail.com>
2021-05-05 22:49:39 -0500

    AdminRestaurante - Avance

---
+++ src/main/java/com/example/tarea4_grupo2/repository/CuponesRepository.java
@@ -0,0 +1,7 @@
+package com.example.tarea4_grupo2.repository;
+
+import com.example.tarea4_grupo2.entity.Cupones;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CuponesRepository extends JpaRepository<Cupones,Integer> {
+}
