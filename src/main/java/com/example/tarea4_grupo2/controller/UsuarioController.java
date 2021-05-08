53c44054101fca24ffe406475b95cab977ab9c7b
Rubi Polanco <a20183238@pucp.edu.pe>
2021-05-08 12:37:15 -0500

    registro cliente (parcial)

--- src/main/java/com/example/tarea4_grupo2/controller/UsuarioController.java
+++ src/main/java/com/example/tarea4_grupo2/controller/UsuarioController.java
@@ -4,9 +4,14 @@ import com.example.tarea4_grupo2.entity.Usuario;
 import com.example.tarea4_grupo2.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 @Controller
 @RequestMapping("/cliente")
@@ -21,8 +26,46 @@ public class UsuarioController {
     }
 
     @PostMapping("/guardarNuevo")
-    public String guardarCliente(Usuario usuario){
-        return "redirect:/cliente/nuevo";
+    public String guardarCliente(@RequestParam("nombres") String nombres,
+                                 @RequestParam("apellidos") String apellidos,
+                                 @RequestParam("email") String email,
+                                 @RequestParam("dni") String dni,
+                                 @RequestParam("telefono") Integer telefono,
+                                 @RequestParam("fechaNacimiento") String fechaNacimiento,
+                                 @RequestParam("sexo") String sexo,
+                                 @RequestParam("direccion") String direccion,
+                                 @RequestParam("distrito") String distrito,
+                                 @RequestParam("contraseniaHash") String contraseniaHash,
+                                 @RequestParam("password2") String pass2,
+                                Model model){
+
+        System.out.println(nombres + apellidos + email + dni + telefono + fechaNacimiento);
+        System.out.println(fechaNacimiento);
+        if(contraseniaHash.equals(pass2)){
+            Usuario usuario = new Usuario();
+            usuario.setNombre(nombres);
+            usuario.setApellidos(apellidos);
+            usuario.setEmail(email);
+            usuario.setTelefono(telefono);
+            usuario.setSexo(sexo);
+            usuario.setContraseniaHash(contraseniaHash);
+            usuario.setRol("Cliente");
+            usuario.setCuentaActiva(1);
+            usuario.setDni(dni);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            try{
+                usuario.setFechaNacimiento(sdf.parse(fechaNacimiento));
+                System.out.println(fechaNacimiento);
+            }catch(ParseException e){
+                e.printStackTrace();
+                return "cliente/registroCliente";
+            }
+            usuarioRepository.save(usuario);
+            return "cliente/confirmarCuenta";
+        }else{
+            return "cliente/registroCliente";
+        }
+
     }
 
     @GetMapping("/reportes")
