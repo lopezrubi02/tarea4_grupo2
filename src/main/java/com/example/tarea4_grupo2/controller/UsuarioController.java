3b142e3ef333d79a9e03d6723de1b02d4659b033
LiziAC05 <lizbeth_aqcast@hotmail.com>
2021-05-10 19:10:48 -0500

    Hasta donde pude. Ya veré luego los detalles

--- src/main/java/com/example/tarea4_grupo2/controller/UsuarioController.java
+++ src/main/java/com/example/tarea4_grupo2/controller/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.example.tarea4_grupo2.controller;
 
+import com.example.tarea4_grupo2.dto.TiempoMedio_ClienteDTO;
+import com.example.tarea4_grupo2.dto.Top3Platos_ClientesDTO;
+import com.example.tarea4_grupo2.dto.Top3Restaurantes_ClienteDTO;
 import com.example.tarea4_grupo2.entity.Categorias;
 import com.example.tarea4_grupo2.entity.Direcciones;
 import com.example.tarea4_grupo2.entity.Pedidos;
@@ -11,11 +14,10 @@ import com.example.tarea4_grupo2.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -38,15 +40,13 @@ public class UsuarioController {
     PedidosRepository pedidosRepository;
 
     @GetMapping("/paginaprincipal")
-    public String paginaprincipal()
-    {
+    public String paginaprincipal() {
         return "cliente/paginaPrincipal";
     }
 
 
-
     @GetMapping("/nuevo")
-    public String nuevoCliente(){
+    public String nuevoCliente() {
         return "cliente/registroCliente";
     }
 
@@ -62,11 +62,11 @@ public class UsuarioController {
                                  @RequestParam("distrito") String distrito,
                                  @RequestParam("contraseniaHash") String contraseniaHash,
                                  @RequestParam("password2") String pass2,
-                                Model model){
+                                 Model model) {
 
         System.out.println(nombres + apellidos + email + dni + telefono + fechaNacimiento);
         System.out.println(fechaNacimiento);
-        if(contraseniaHash.equals(pass2)){
+        if (contraseniaHash.equals(pass2)) {
             Usuario usuario = new Usuario();
             usuario.setNombre(nombres);
             usuario.setApellidos(apellidos);
@@ -78,10 +78,10 @@ public class UsuarioController {
             usuario.setCuentaActiva(1);
             usuario.setDni(dni);
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            try{
+            try {
                 usuario.setFechaNacimiento(sdf.parse(fechaNacimiento));
                 System.out.println(fechaNacimiento);
-            }catch(ParseException e){
+            } catch (ParseException e) {
                 e.printStackTrace();
                 return "cliente/registroCliente";
             }
@@ -99,42 +99,72 @@ public class UsuarioController {
             direccionesRepository.save(direccionactual);
 
             return "cliente/confirmarCuenta";
-        }else{
+        } else {
             return "cliente/registroCliente";
         }
 
     }
 
     @GetMapping("/reportes")
-    public String reportesCliente(Model model, @RequestParam("idcliente") int idcliente,
+    public String reportesCliente(Model model, @RequestParam("idusuarios") int idusuarios,
                                   @RequestParam("anio") int anio,
-                                  @RequestParam("mes") int mes){
-        model.addAttribute("listaTop3Restaurantes", pedidosRepository.obtenerTop3Restaurantes(idcliente, anio, mes));
-        model.addAttribute("listaTop3Platos", pedidosRepository.obtenerTop3Platos(idcliente, anio, mes));
-        model.addAttribute("listaPromedioTiempo", pedidosRepository.obtenerTiemposPromedio(idcliente, anio, mes));
-        model.addAttribute("listaHistorialConsumo", pedidosRepository.obtenerHistorialConsumo(idcliente, anio, mes));
+                                  @RequestParam("mes") int mes, @ModelAttribute("cliente") Usuario cliente) {
+        idusuarios = 7;
+        Optional<Usuario> optUsuario = usuarioRepository.findById(idusuarios);
+        if (optUsuario.isPresent()) {
+            cliente = optUsuario.get();
+
+            List<Top3Restaurantes_ClienteDTO> top3Restaurantes_clienteDTOS = pedidosRepository.obtenerTop3Restaurantes(idusuarios, anio, mes);
+            List<Top3Platos_ClientesDTO> top3Platos_clientesDTOS = pedidosRepository.obtenerTop3Platos(idusuarios, anio, mes);
+            List<TiempoMedio_ClienteDTO> tiempoMedio_clienteDTOS = pedidosRepository.obtenerTiemposPromedio(idusuarios, anio, mes);
+
+            for(Top3Restaurantes_ClienteDTO t : top3Restaurantes_clienteDTOS){
+                System.out.println(t.getRestaurante());
+            }
+            model.addAttribute("cliente", cliente);
+            model.addAttribute("listaTop3Restaurantes",top3Restaurantes_clienteDTOS );
+            model.addAttribute("listaTop3Platos", top3Platos_clientesDTOS);
+            model.addAttribute("listaPromedioTiempo", tiempoMedio_clienteDTOS);
+            model.addAttribute("listaHistorialConsumo", pedidosRepository.obtenerHistorialConsumo(idusuarios, anio, mes));
+            return "cliente/reportes";
+        } else {
+            return "redirect:/cliente/miperfil";
+        }
+    }
+
+    @PostMapping("/recepcionCliente")
+    public String recepcionCliente(@ModelAttribute("cliente") @Valid Usuario cliente, @RequestParam("idusuarios") int idusuarios,
+                                   @RequestParam("anio") int anio,
+                                   @RequestParam("mes") int mes, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("listaTop3Restaurantes", pedidosRepository.obtenerTop3Restaurantes(idusuarios, anio, mes));
+            model.addAttribute("listaTop3Platos", pedidosRepository.obtenerTop3Platos(idusuarios, anio, mes));
+            model.addAttribute("listaPromedioTiempo", pedidosRepository.obtenerTiemposPromedio(idusuarios, anio, mes));
+            model.addAttribute("listaHistorialConsumo", pedidosRepository.obtenerHistorialConsumo(idusuarios, anio, mes));
+            System.out.println("IDCliente: " + idusuarios + "Mes: " + mes + "Anio: " + anio);
+        }
         return "cliente/reportes";
     }
 
     @GetMapping("/realizarpedido")
-    public String realizarpedido(Model model){
+    public String realizarpedido(Model model) {
 
         int idusuarioactual = 7;
 
         List<Direcciones> listadireccionescliente = direccionesRepository.findAllByUsuariosIdusuariosEquals(idusuarioactual);
         List<Categorias> listacategorias = categoriasRepository.findAll();
         model.addAttribute("listacategorias", listacategorias);
-        model.addAttribute("listadirecciones",listadireccionescliente);
+        model.addAttribute("listadirecciones", listadireccionescliente);
 
 
         return "cliente/realizar_pedido_cliente";
     }
 
     @GetMapping("/miperfil")
-    public String miperfil(Model model){
+    public String miperfil(Model model) {
         int idusuario = 7;
         List<Direcciones> listadireccionescliente = direccionesRepository.findAllByUsuariosIdusuariosEquals(idusuario);
-        model.addAttribute("listadirecciones",listadireccionescliente);
+        model.addAttribute("listadirecciones", listadireccionescliente);
         Optional<Usuario> optional = usuarioRepository.findById(idusuario);
         Usuario usuario = optional.get();
         model.addAttribute("usuario", usuario);
@@ -143,7 +173,7 @@ public class UsuarioController {
     }
 
     @PostMapping("/miperfil")
-    public String updatemiperfil(Usuario usuarioRecibido){
+    public String updatemiperfil(Usuario usuarioRecibido) {
         System.out.println(usuarioRecibido.getIdusuarios());
         Optional<Usuario> optusuario = usuarioRepository.findById(usuarioRecibido.getIdusuarios());
         Usuario usuariodb = optusuario.get();
@@ -160,7 +190,7 @@ public class UsuarioController {
 
     @GetMapping("/borrardireccion")
     public String borrardireccion(@RequestParam("iddireccion") int iddireccion,
-                                  Model model){
+                                  Model model) {
 
         direccionesRepository.deleteById(iddireccion);
 
@@ -168,14 +198,14 @@ public class UsuarioController {
     }
 
     @GetMapping("/agregardireccion")
-    public String agregardireccion(){
+    public String agregardireccion() {
 
         return "cliente/registrarNuevaDireccion";
     }
 
     @PostMapping("/guardarnuevadireccion")
     public String guardarnuevadireccion(@RequestParam("direccion") String direccion,
-                                        @RequestParam("distrito") String distrito){
+                                        @RequestParam("distrito") String distrito) {
 
         int idusuario = 7;
 
--- src/main/java/com/example/tarea4_grupo2/dto/TiempoMedio_ClienteDTO.java
+++ src/main/java/com/example/tarea4_grupo2/dto/TiempoMedio_ClienteDTO.java
@@ -2,5 +2,5 @@ package com.example.tarea4_grupo2.dto;
 
 public interface TiempoMedio_ClienteDTO {
     String getNombrerestaurante();
-    int getTiempopromedio();
+    Double getTiempopromedio();
 }
