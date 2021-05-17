d2d31c16207730f7e35786b8831b9e7aa628cd48
Emerson Munoz <munoz.e@pucp.edu.pe>
2021-05-16 20:16:08 -0500

    a

--- src/main/java/com/example/tarea4_grupo2/dto/PedidosReporteDto.java
+++ src/main/java/com/example/tarea4_grupo2/dto/PedidosReporteDto.java
@@ -1,14 +1,10 @@
 package com.example.tarea4_grupo2.dto;
 
-import java.util.Date;
-
-public interface PedidosReporteDto {
-    int getnumeropedido();
-    Date getfechahorapedido();
-    String getnombre();
-    String getapellidos();
-    float getmontototal();
-    String getnombreplato();
-    String getmetodo();
-    String getdistrito();
+public interface PedidosReporteDTO {
+    int getIdpedidos();
+    float getMontototal();
+    int getComisionrepartidor();
+    int getCalificacionrepartidor();
+    String getNombre();
+    String getDistrito();
 }
