407bbc4eb922fd193ee0f6545615d3ac6346849d
joseluismunozzuta <joseluismunozzuta@gmail.com>
2021-06-10 19:49:05 -0500

    cambios

--- src/main/java/com/example/tarea4_grupo2/repository/DireccionesRepository.java
+++ src/main/java/com/example/tarea4_grupo2/repository/DireccionesRepository.java
@@ -12,11 +12,11 @@ public interface DireccionesRepository extends JpaRepository<Direcciones, Intege
 
     List<Direcciones> findAllByUsuariosIdusuariosEquals(int idusuario);
 
-    @Query(value = "select * from Direcciones where iddirecciones = ?1", nativeQuery = true)
+    @Query(value = "select * from Direcciones d where d.iddirecciones = ?1", nativeQuery = true)
     Direcciones findDireccionById(int id);
 
     //TODO: usar esto en el controller del cliente, solo remplazar
-    @Query(value = "select * from Direcciones where iddirecciones = ?1 and usuarios_idusuarios = ?2", nativeQuery = true)
+    @Query(value = "select * from Direcciones d where d.iddirecciones = ?1 and d.usuarios_idusuarios = ?2", nativeQuery = true)
     Direcciones findDireccionesByIddireccionesAndUsuariosIdusuariosEquals(int iddireccion, int idcliente);
 
     Direcciones findByUsuariosIdusuarios(int usuarios_idusuarios);
